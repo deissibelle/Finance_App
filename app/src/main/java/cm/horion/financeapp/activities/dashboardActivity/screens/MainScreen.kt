7b1523a22e2278e93d82df861b5246f3fd408d10
deissibelle <deissibelle@gmail.com>
2026-04-29 12:49:52 +0100

    feat(components):added expensesItem

--- app/src/main/java/cm/horion/financeapp/activities/dashboardActivity/screens/MainScreen.kt
+++ app/src/main/java/cm/horion/financeapp/activities/dashboardActivity/screens/MainScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -14,6 +15,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import cm.horion.financeapp.activities.dashboardActivity.components.ActionButtonRow
 import cm.horion.financeapp.activities.dashboardActivity.components.CardSection
+import cm.horion.financeapp.activities.dashboardActivity.components.ExpenseItem
 import cm.horion.financeapp.activities.dashboardActivity.components.HeaderSection
 import cm.horion.financeapp.domain.ExpenseDomain
 
@@ -39,7 +41,10 @@ fun MainScreen(
         ){
             item { HeaderSection() }
             item { CardSection(onclick = onCardClick) }
-            item { ActionButtonRow() }
+            item { ActionButtonRow()  }
+            items(expenses){
+                item -> ExpenseItem(item = item)
+            }
 
         }
     }
@@ -52,10 +57,10 @@ fun MainScreen(
 fun MainScreenPreview() {
 
     val expenses = listOf(
-        ExpenseDomain("Restaurant", 1000.0, "img1", "17 jun 2025 19:15"),
-        ExpenseDomain("McDonald",73.12,"img2","11 jun 2025 19:30"),
-        ExpenseDomain("Cinema",23.47,"img3","17 mars 2025 19:15"),
-        ExpenseDomain("Restaurant",573.12,"img4","20 avril 2025 21:15")
+        ExpenseDomain("Restaurant", 10.0, "restaurant", "17 jun 2025 19:15"),
+        ExpenseDomain("McDonald",73.12,"mcdonald","11 jun 2025 19:30"),
+        ExpenseDomain("Cinema",23.47,"cinema","17 mars 2025 19:15"),
+        ExpenseDomain("Restaurant",573.12,"restaurant","20 avril 2025 21:15")
     )
     MainScreen(expenses= expenses)
 }
